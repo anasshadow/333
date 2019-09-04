45e30e39ce628b5faaea60889e17345e0619ed31
anasshadow <54816572+anasshadow@users.noreply.github.com>
2019-09-04 10:28:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const client10 = new Discord.Client();
 console.log("Scrpit By Shadow");
 const myid = ['524218118462242816'];
 const prefix = ['!'];
-const mess = ['هلا بالزعامة '];
+const mess = ['هانتيkanty '];
 
 client.on("ready", () => {
 let channel =     client.channels.get("618079138376450198")
