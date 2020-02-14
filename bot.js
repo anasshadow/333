13807d10adb3a02ed1b6357c7443168e1379c804
anasshadow <54816572+anasshadow@users.noreply.github.com>
2020-02-14 23:34:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,60 +38,6 @@ client.on('message', message => {
     }
 });
 
-
-client.on('message', message => {
-if (message.content === prefix+'spam') {
-        let letters = "ههههههخول".split("");
-        let spaces = " ".repeat(10);
-        let last = "";
-        let messages = 0;
- 
-        setInterval(() => {
-            let str = "";
-            let num = Math.floor(Math.random() * 10);
-            for(let i = 0; i < num; i++) {
-                str += letters[Math.floor(Math.random() * letters.length)];
- 
-                if(i === num - 1) {
-                    if(last === "") {
-                        messages += 1;
-                        last = `${str} ${spaces.slice(str.length + 1)}`;
-                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
-                    } else {
-                        messages += 1;
-                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
-                        last = spaced;
-                        return message.channel.send(spaced);
-                    }
-                }
-            }
-        }, 6000);
-}
-});
-
-
-
-
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "s") {
-let rank = message.guild.member(message.author).roles.find('name', 'spamer'); //لازم تعمل الرتبه دي في السيرفر  spamer
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
-
  
     
 client.login(process.env.BOT_TOKEN);
