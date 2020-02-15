531c261fd90b75bb5083af5f4a35b088624c6722
anasshadow <54816572+anasshadow@users.noreply.github.com>
2020-02-15 20:15:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,59 +8,86 @@ client.on('message', message => {
         message.channel.send("#daily")
     }
 });
-
-
-client.on('message', message => {
-    if(message.content === 'credit'){
-        message.channel.send("#credit")
-    }
-});
-
-
-client.on('message', message => {
-if (message.content === prefix+'spam') {
-        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".split("");
-        let spaces = " ".repeat(10);
-        let last = "";
-        let messages = 0;
- 
-        setInterval(() => {
-            let str = "";
-            let num = Math.floor(Math.random() * 10);
-            for(let i = 0; i < num; i++) {
-                str += letters[Math.floor(Math.random() * letters.length)];
- 
-                if(i === num - 1) {
-                    if(last === "") {
-                        messages += 1;
-                        last = `${str} ${spaces.slice(str.length + 1)}`;
-                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
-                    } else {
-                        messages += 1;
-                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
-                        last = spaced;
-                        return message.channel.send(spaced);
-                    }
-                }
-            }
-        }, 6000);
-}
-});
-
-
 client2.on('message', message => {
     if(message.content === 'daily'){
         message.channel.send("#daily")
     }
 });
+client3.on('message', message => {
+    if(message.content === 'daily'){
+        message.channel.send("#daily")
+    }
+});
+client4.on('message', message => {
+    if(message.content === 'daily'){
+        message.channel.send("#daily")
+    }
+});
+client5.on('message', message => {
+    if(message.content === 'daily'){
+        message.channel.send("#daily")
+    }
+});  
 
 
+
+
+client.on('message', message => {
+    if(message.content === 'credit'){
+        message.channel.send("#credit")
+    }
+});
 client2.on('message', message => {
     if(message.content === 'credit'){
         message.channel.send("#credit")
     }
 });
+client3.on('message', message => {
+    if(message.content === 'credit'){
+        message.channel.send("#credit")
+    }
+});
+client4.on('message', message => {
+    if(message.content === 'credit'){
+        message.channel.send("#credit")
+    }
+});
+client5.on('message', message => {
+    if(message.content === 'credit'){
+        message.channel.send("#credit")
+    }
+});
 
+
+client1.on('message', message => {
+if (message.content === prefix+'spam') {
+        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)];
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+});
 
 client2.on('message', message => {
 if (message.content === prefix+'spam') {
@@ -93,7 +120,104 @@ if (message.content === prefix+'spam') {
 });
 
 
+client3.on('message', message => {
+if (message.content === prefix+'spam') {
+        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)];
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+});
 
-client2.login(process.env.BOT_TOKEN2);
+
+client4.on('message', message => {
+if (message.content === prefix+'spam') {
+        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)];
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+});
+
+
+client5.on('message', message => {
+if (message.content === prefix+'spam') {
+        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)];
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+});
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
-
+client2.login(process.env.BOT_TOKEN2);
+client3.login(process.env.BOT_TOKEN3);
+client4.login(process.env.BOT_TOKEN4);
+client5.login(process.env.BOT_TOKEN5);
