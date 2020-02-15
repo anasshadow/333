e4c247e9be674ce5c020a0ce112531669c8357a5
anasshadow <54816572+anasshadow@users.noreply.github.com>
2020-02-15 15:10:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,27 +67,5 @@ if (message.content === prefix+'spam') {
 });
 
 
-
-
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "s") {
-let rank = message.guild.member(message.author).roles.find('name', 'spamer'); //لازم تعمل الرتبه دي في السيرفر  spamer
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
- 
     
 client.login(process.env.BOT_TOKEN);
