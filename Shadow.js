9f3c77ca3251eec658770819546321fb3f065247
anasshadow <54816572+anasshadow@users.noreply.github.com>
2020-02-15 23:38:09 +0200

    Update Shadow.js

--- Shadow.js
+++ Shadow.js
@@ -1,14 +1,5 @@
 const http = require('http');
 const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://shadowspama.glitch.me/`);
-}, 280000);
- 
 
 const Discord = require('discord.js');
 const initcmd = "-";
