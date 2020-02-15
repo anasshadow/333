8891a620da86f86c8a75a7f7981698164715073e
anasshadow <54816572+anasshadow@users.noreply.github.com>
2020-02-15 15:12:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,36 +36,7 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-if (message.content === prefix+'spam') {
-        let letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789".split("");
-        let spaces = " ".repeat(10);
-        let last = "";
-        let messages = 0;
- 
-        setInterval(() => {
-            let str = "";
-            let num = Math.floor(Math.random() * 10);
-            for(let i = 0; i < num; i++) {
-                str += letters[Math.floor(Math.random() * letters.length)];
- 
-                if(i === num - 1) {
-                    if(last === "") {
-                        messages += 1;
-                        last = `${str} ${spaces.slice(str.length + 1)}`;
-                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
-                    } else {
-                        messages += 1;
-                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
-                        last = spaced;
-                        return message.channel.send(spaced);
-                    }
-                }
-            }
-        }, 6000);
-}
-});
 
-
+ 
     
 client.login(process.env.BOT_TOKEN);
